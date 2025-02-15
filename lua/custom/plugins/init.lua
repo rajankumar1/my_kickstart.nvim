4e4f1a92eff9933fefd55c8d401b3416cedde833
Rajan Kumar <rajanprakash11@gmail.com>
2025-02-15 15:30:37 +0530

    Added custom plugin
    Identification: 0

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,16 @@
 -- See the kickstart.nvim README for more information
 return {
   -- This is just a test. I will add more stuffs below later.
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
 }
